20ea15a374470265bd6f5bc35a1d3f610425126a
chaoyu <49432715+chaoyuim@users.noreply.github.com>
2023-06-21 22:57:03 +0200

    Update config_ords.sh

--- ordsForXE/config_ords.sh
+++ ordsForXE/config_ords.sh
@@ -3,7 +3,6 @@ ords install --admin-user "sys as sysdba" \
              --db-tns-dir "/opt/oracle/network/admin" \
              --db-tns-alias "XEPDB1" \
              --feature-db-api true \
-             --db-pool db1 \
              --feature-sdw true \
              --feature-rest-enabled-sql true \
              --password-stdin << EOF
